13eb17818c9def49e70b22195acf160c323e6b69
Emem Isaac <isaacemem@gmail.com>
2019-03-29 17:53:10 +0100

    Added link to courses because Modules belong to courses

--- app/Module.php
+++ app/Module.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+    protected $fillable = [
+        'course_key',
+        'name'
+    ];
+
+    public function course(){
+        return $this->belongsTo('App\Course', 'course_key');
+    }
 }
