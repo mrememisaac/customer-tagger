a0367cbb7ce35925d54236e831624a2aa132f82e
Emem Isaac <isaacemem@gmail.com>
2019-03-29 23:30:21 +0100

    Added extra seeeders

--- database/seeds/iPSDevTestSeeder.php
+++ database/seeds/iPSDevTestSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use App\Module;
+use App\Course;
+use App\Contact;
+use App\User;
 
 class iPSDevTestSeeder extends Seeder
 {
@@ -12,7 +15,33 @@ class iPSDevTestSeeder extends Seeder
      */
     public function run()
     {
-
+        User::insert(
+            [
+                'email' => 'isaacemem@gmail.com',
+                'password' => '12345',
+                'name' => 'Emem Isaac',
+            ]
+        );
+        Contact::insert(
+            [
+                'email' => 'isaacemem@gmail.com',
+            ]
+        );
+        Course::insert([
+            [
+                'course_key' => 'ipa',
+                'name' => 'iPhone Photography Academy'
+            ],
+            [
+                'course_key' => 'iea',
+                'name' => 'iPhone Editing Academy'
+            ],
+            [
+                'course_key' => 'iaa',
+                'name' => 'iPhone Art Academy'
+            ]
+        ]);
+        
         for ($i = 1; $i <= 7; $i++){
             Module::insert([
                 [
