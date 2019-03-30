295351fa3a58c501b61a0bc08ffde2484b46ee8a
Emem Isaac <isaacemem@gmail.com>
2019-03-30 22:55:26 +0100

    Added routes for testing the local infusionsoft controller

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/authorize_infusionsoft', 'InfusionsoftController@authorizeInfusionsoft')->name('infusionsoft.authorize');
 Route::post('/authorize_infusionsoft', 'InfusionsoftController@authorizeInfusionsoft')->name('infusionsoft.authorize');
-Route::get('/create_contact', 'InfusionsoftController@testInfusionsoftIntegrationCreateContact');
 
+Route::get('/create_contact', 'InfusionsoftController@testInfusionsoftIntegrationCreateContact');
 Route::get('/infusionsoft_test_get_by_email/{email}', 'InfusionsoftController@testInfusionsoftIntegrationGetEmail')->name('api.infusionsoft_test_email');
 Route::get('/infusionsoft_test_add_tag/{contact_id}/{tag_id}', 'InfusionsoftController@testInfusionsoftIntegrationAddTag')->name('api.infusionsoft_test_tag');
 Route::get('/infusionsoft_test_get_all_tags', 'InfusionsoftController@testInfusionsoftIntegrationGetAllTags')->name('api.infusionsoft_test_get_all_tags');
+
+Route::get('/mcreate_contact', 'MockInfusionsoftController@testInfusionsoftIntegrationCreateContact');
+Route::get('/minfusionsoft_test_get_by_email/{email}', 'MockInfusionsoftController@testInfusionsoftIntegrationGetEmail')->name('api.infusionsoft_test_email');
+Route::get('/minfusionsoft_test_add_tag/{contact_id}/{tag_id}', 'MockInfusionsoftController@testInfusionsoftIntegrationAddTag')->name('api.infusionsoft_test_tag');
+Route::get('/minfusionsoft_test_get_all_tags', 'MockInfusionsoftController@testInfusionsoftIntegrationGetAllTags')->name('api.infusionsoft_test_get_all_tags');
